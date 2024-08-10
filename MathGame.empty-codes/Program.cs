ed8d60d59493953a9737bac98826d2e194193c6f
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-08-10 15:21:36 +0100

    - Fulfilled first 3 math game requirements - Added comments

--- MathGame.empty-codes/Program.cs
+++ MathGame.empty-codes/Program.cs
@@ -6,7 +6,7 @@ menuChoice = int.Parse(Console.ReadLine());
 
 switch (menuChoice) {
     case 1:
-        getOperatorChoice();
+        getQuestions(getOperatorChoice());
         break;
     case 2:
         Console.WriteLine();
@@ -16,40 +16,85 @@ switch (menuChoice) {
         break;  
 }
 
-void getOperatorChoice()
+char getOperatorChoice()
 {
-    Random random = new Random();
-    int a = 0, b = 0;
     char operatorChoice;
     Console.WriteLine("Choose an operation using the options below:");
     Console.WriteLine("'+' for addition\n'-' for subtraction\n'*' for multiplication\n'/' for division");
-    operatorChoice = char.Parse(Console.ReadLine());
-
-    switch(operatorChoice)
-    {
-        case '+':
-            getAdditionQuestions();
-            break;
-        case '-':
-            getSubtractionQuestions();
-            break;
-        case '*':
-            getMultiplicationQuestions();
-            break;
-        case '/':
-            getDivisionQuestions();
-            break;
-        default:
-            Console.WriteLine("Error!, Input only the specified symbols");
-            break;
-    }
+    return operatorChoice = char.Parse(Console.ReadLine());
 }
 
-void getAdditionQuestions()
+// generates five questions using the operator chosen
+void getQuestions(char operatorChoice)
 {
+    int a;
+    int b;
+    int correctCount = 0;
+    int userAnswer;
+    int correctAnswer;
+
+    //  generates and displays the five questions
+    for (int i = 1; i < 6; i++)
+    {
+        // randomly generates operands a and b
+        Random random = new Random();
+        a = random.Next(0, 101);
+        b = random.Next(1, 101); // starts at 1 since division by 0 results in an error
+
+        switch(operatorChoice)
+        {
+            case '+':
+                Console.Write($"Question {i}/5: {a} + {b} = ");
+                userAnswer = int.Parse(Console.ReadLine());
+                correctAnswer = a + b;
+                isCorrect(userAnswer, correctAnswer);
+                break;
+            case '-':
+                Console.Write($"Question {i}/5: {a} - {b} = ");
+                userAnswer = int.Parse(Console.ReadLine());
+                correctAnswer = a - b;
+                isCorrect(userAnswer, correctAnswer);
+                break;
+            case '*':
+                Console.Write($"Question {i}/5: {a} * {b} = ");
+                userAnswer = int.Parse(Console.ReadLine());
+                correctAnswer = a * b;
+                isCorrect(userAnswer, correctAnswer);
+                break;
+            case '/':
+                // ensures division results in integers only
+                while(a % b != 0)
+                {
+                    b = random.Next(1, 101);
+                    if (a % b == 0) break;
+                }
+                Console.Write($"Question {i}/5: {a} / {b} = ");
+                userAnswer = int.Parse(Console.ReadLine());
+                correctAnswer = a / b;
+                isCorrect(userAnswer, correctAnswer);
+                break;
+            default:
+                Console.WriteLine("");
+                break;
+        } 
+    }
+
+    void isCorrect(int userAnswer, int correctAnswer)
+    {
+        if (userAnswer == correctAnswer)
+        {
+            Console.WriteLine("Correct!");
+            correctCount++;
+        }
+
+        if (userAnswer != correctAnswer)
+        {
+            Console.WriteLine($"Wrong! {a} {operatorChoice} {b} = {correctAnswer}");
+        }
+    }
+
+    Console.WriteLine($"You got {correctCount}/5 questions correct!");
 
 }
-
-
 
 Console.ReadLine();
