e0fc17697dd0c85ebc4801dcd87fd72e91879900
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-08-10 14:06:05 +0100

    - Uncommented code

--- MathGame.empty-codes/Program.cs
+++ MathGame.empty-codes/Program.cs
@@ -1,53 +1,54 @@
 ﻿
 int menuChoice;
+Console.WriteLine("Welcome to emptycode's math quiz game! :)");
+Console.Write("Enter 1 to Start or 2 to View Previous Games:");
+menuChoice = int.Parse(Console.ReadLine());
 
-char op = '+';
-Console.WriteLine($"2 {op} 4 = ");
+switch (menuChoice) {
+    case 1:
+        getOperatorChoice();
+        break;
+    case 2:
+        Console.WriteLine();
+        break;
+    default:
+        Console.WriteLine("Error!, Enter 1 to Start or 2 to View Previous Games");
+        break;  
+}
 
-//Console.WriteLine("Welcome to emptycode's math quiz game! :)");
-//Console.Write("Enter 1 to Start or 2 to View Previous Games:");
-//menuChoice = int.Parse(Console.ReadLine());
+void getOperatorChoice()
+{
+    Random random = new Random();
+    int a = 0, b = 0;
+    char operatorChoice;
+    Console.WriteLine("Choose an operation using the options below:");
+    Console.WriteLine("'+' for addition\n'-' for subtraction\n'*' for multiplication\n'/' for division");
+    operatorChoice = char.Parse(Console.ReadLine());
 
-//switch (menuChoice) {
-//    case 1:
-//        getOperatorChoice();
-//        break;
-//    case 2:
-//        Console.WriteLine();
-//        break;
-//    default:
-//        Console.WriteLine("Error!, Enter 1 to Start or 2 to View Previous Games");
-//        break;  
-//}
+    switch(operatorChoice)
+    {
+        case '+':
+            getAdditionQuestions();
+            break;
+        case '-':
+            getSubtractionQuestions();
+            break;
+        case '*':
+            getMultiplicationQuestions();
+            break;
+        case '/':
+            getDivisionQuestions();
+            break;
+        default:
+            Console.WriteLine("Error!, Input only the specified symbols");
+            break;
+    }
+}
 
-//char getOperatorChoice()
-//{
-//    Random random = new Random();
-//    int a = 0, b = 0;
-//    char operatorChoice;
-//    Console.WriteLine("Choose an operation using the options below:");
-//    Console.WriteLine("'+' for addition\n'-' for subtraction\n'*' for multiplication\n'/' for division");
-//    operatorChoice = char.Parse(Console.ReadLine());
+void getAdditionQuestions()
+{
 
-//    switch(operatorChoice)
-//    {
-//        case '+':
-//            ope
-//            break;
-//        case '-':
-//            break;
-//        case '*':
-//            break;
-//        case '/':
-//            break;
-//        default:
-//            Console.WriteLine("Error!, Input only the specified symbols");
-//            break;
-//    }
-
-    
-    
-//}
+}
 
 
 
